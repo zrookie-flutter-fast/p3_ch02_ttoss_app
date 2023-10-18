835910ea72bd30bb57072b6dc2e6fc57c3c2c964
zRooKie <zrookie@naver.com>
2023-10-18 11:38:51 +0900

    07. List, Map, Set 데이터 구조

--- lib/screen/main/tab/home/bank_accounts_dummy.dart
+++ lib/screen/main/tab/home/bank_accounts_dummy.dart
@@ -11,6 +11,15 @@ final bankAccountToss = BankAccount(bankShinhan, 5000000);
 final bankAccountKakao =
     BankAccount(bankShinhan, 7000000, accountTypeName: "입출금통장");
 
+// 1. List: 객체 순서를 가지고 있는 배열
+// 중복 혀용
+
+// main() {
+//   for (final item in bankAccounts) {
+//     print(item.accountTypeName);
+//   }
+// }
+
 final bankAccounts = [
   bankAccountShinhan1,
   bankAccountShinhan2,
@@ -18,3 +27,35 @@ final bankAccounts = [
   bankAccountToss,
   bankAccountKakao,
 ];
+
+// 2. Map: key와 value를 가지고 있는 배열 (순서가 보장되지 않는다.)
+// 중복 허용 않음
+
+// main() {
+//   // final shinhanBank = bankMap["shinhan1"];
+//   // print(shinhanBank == bankAccountShinhan1);
+//
+//   for (final entry in bankMap.entries) {
+//     print(entry.key + ":" + (entry.value.accountTypeName ?? ""));
+//   }
+// }
+
+final bankMap = {
+  "shinhan1": bankAccountShinhan1,
+  "shinhan2": bankAccountShinhan2
+};
+
+// 3. Set (존재 여부 판단시 많이 사용)
+// 중복 허용 않음
+
+// main() {
+//   print(bankSet.contains(bankAccountShinhan1));
+// }
+
+final bankSet = {
+  bankAccountShinhan1,
+  bankAccountShinhan2,
+  bankAccountShinhan3,
+  bankAccountToss,
+  bankAccountKakao,
+};
